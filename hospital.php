599bed98b592bb6474ae083f130c965f878914d1
Krishna Kumar M <65696644+KrishnaKumar2002@users.noreply.github.com>
2022-03-27 08:33:46 +0530

    Add files via upload

--- hospital.php
+++ hospital.php
@@ -88,7 +88,7 @@ Enter patient id:
 <tr>
     <td>Photo:</td>
     <td> Select Image File to Upload:
-    <input type="file" name="file">
+    <input type="file" name="fileToUpload">
     </td>
 </tr>
 <tr>
@@ -124,34 +124,60 @@ Enter patient id:
 
 <?php
     
-    $db=mysqli_connect($servername,$username,$password,$dbname);
-    $fileName = basename($_FILES["file"]);
-    if(isset($_POST["submit"]) && !empty($_FILES["file"])){
-        // Allow certain file formats
-        $allowTypes = array('jpg','png','jpeg','gif','pdf');
-        if(in_array($allowTypes)){
-            // Upload file to server
-           
-                $insert = $db->query("INSERT into patient (file_name) VALUES ('".$fileName."')");
-                if($insert){
-                    $statusMsg = "The file has been uploaded successfully.";
-                }else{
-                    $statusMsg = "File upload failed, please try again.";
-                } 
-            }
-         }
-        else{
-        $statusMsg = 'Please select a file to upload.';
-          }
-    
-    // Display status message
-    echo $statusMsg;
+$db=mysqli_connect($servername,$username,$password,$dbname);
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
+  }
+}
+
+// Check if file already exists
+if (file_exists($target_file)) {
+  echo "Sorry, file already exists.";
+  $uploadOk = 0;
+}
+
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
+
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"]))." has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
 
 
 
     if(isset($_POST["sub1"]))
     {
-        mysqli_query($conn,"insert into patient values('$_POST[pati]','$_POST[name]',$_POST[ph],'$_POST[addr]','$_POST[ill]')");
+        mysqli_query($conn,"insert into patient values('$_POST[pati]','$_POST[name]',$_POST[ph],'$_POST[addr]','$_POST[ill]','$_POST[fileToUpload]')");
         echo "record inserted succesfully";
     }
     if(isset($_POST["sub2"]))
@@ -161,7 +187,7 @@ Enter patient id:
     }
     if(isset($_POST["sub3"]))
     {
-        mysqli_query($conn,"update patient set name='$_POST[name]',ph='$_POST[ph]',addr='$_POST[addr]',ill='$_POST[ill]' where pati='$_POST[pati]'");
+        mysqli_query($conn,"update patient set name='$_POST[name]',ph='$_POST[ph]',addr='$_POST[addr]',ill='$_POST[ill]',fileToUpload='$_POST[fileToUpload]' where pati='$_POST[pati]'");
         echo "updated succesfully";
     }
     
@@ -175,6 +201,8 @@ Enter patient id:
             echo"<th>"; echo"Phone No"; echo"</th>";
             echo"<th>"; echo"Address"; echo"</th>";
             echo"<th>"; echo"Illness"; echo"</th>";
+            echo"<th>"; echo"File"; echo"</th>";
+
             echo"</tr>";
         while($row=mysqli_fetch_array($reg))
         {
@@ -184,7 +212,10 @@ Enter patient id:
             echo"<td>"; echo $row["ph"]; echo"</td>";
             echo"<td>"; echo $row["addr"]; echo"</td>";
             echo"<td>"; echo $row["ill"]; echo"</td>";
-
+            echo"<td>";echo '<a href="uploads/$row["fileToUpload"] target="_blank">view file</a>'; echo"</td>";
+            /*
+            echo"<td>"; echo $row["fileToUpload"]; echo"</td>";
+            */
             echo"</tr>";
         }
         echo"</table>";
@@ -200,6 +231,7 @@ Enter patient id:
             echo"<th>"; echo"Phone No"; echo"</th>";
             echo"<th>"; echo"Address"; echo"</th>";
             echo"<th>"; echo"Illness"; echo"</th>";
+            echo"<th>"; echo"File"; echo"</th>";
 
             echo"</tr>";
         while($row=mysqli_fetch_array($reg))
@@ -210,27 +242,10 @@ Enter patient id:
             echo"<td>"; echo $row["ph"]; echo"</td>";
             echo"<td>"; echo $row["addr"]; echo"</td>";
             echo"<td>"; echo $row["ill"]; echo"</td>";
+            echo"<td>"; echo $row["fileToUpload"]; echo"</td>";
 
             echo"</tr>";
         }
         echo"</table>";
     }
-/*
-    if(isset($_POST["Upload"]))
-    {
-
-        // Get images from the database
-        $query = $db->query("SELECT file_name FROM patient");
-        
-        if($query->num_rows > 0){
-            while($row = $query->fetch_assoc()){
-                $imageURL = 'uploads/'.$row["file_name"];
-        
-            <img src=echo" $imageURL; " alt="" />
-       
-    }
-        }
-        else{ 
-            <p>No image(s) found...</p>
-         }*/
 ?>
